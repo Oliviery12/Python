165d6d97c5e565052755bad612f832054d2afc9c
Olivier Yildiz <olivier_yildiz@o2.pl>
2022-01-22 17:12:53 +0100

    cos

--- cos.py
+++ cos.py
@@ -6,3 +6,8 @@ def test_dodaj():
     assert plik.dodaj(5,2) == 7
     assert plik.dodaj(5,5) == 10
     assert plik.dodaj(5,3) == 8
+
+    assert plik.dodaj(5,6) == 11
+    assert plik.dodaj(5,2) == 7
+    assert plik.dodaj(5,5) == 10
+    assert plik.dodaj(5,3) == 8
